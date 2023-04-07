a176837aa7664cc07c32582061edad987c066730
friedman <eliot.friedmann@etu.u_paris.fr>
2023-04-07 10:05:15 +0200

    Signature des fonctions de base des listes

--- lib/liste.h
+++ lib/liste.h
@@ -11,5 +11,11 @@ struct liste_noeud {
 };
 
 typedef struct liste_noeud liste_noeud;
+typedef struct noeud noeud;
 
+extern liste_noeud creer_liste();
+extern noeud *get_elt(liste_noeud*, char[100]);
+extern noeud *ajouter_elt (liste_noeud*, noeud*);// ordre alphabétique
+extern noeud *supprimer_elt(liste_noeud*, noeud*);
+extern void affiche_liste(liste_noeud*);
 #endif
