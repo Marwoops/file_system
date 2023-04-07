e3ae9473121aa4aadabc70c65081bd6b51e99cbb
azizim <marwan.azizi@etu.u-paris.fr>
2023-04-07 12:22:42 +0200

    signature des fonctions dans arbre.h

--- lib/arbre.h
+++ lib/arbre.h
@@ -19,5 +19,11 @@ typedef struct noeud noeud;
 extern noeud* creer_arbre();
 extern noeud* creer_fichier(noeud* , char [100]);
 extern noeud* creer_dossier(noeud* , char [100], liste_noeud*);
+
+extern noeud* ajouter_noeud(noeud*, noeud*);
+extern void supprimer_noeud(noeud*, noeud*);
+extern void supprimer_noeud_par_nom(noeud*, char[100]);
+
+extern bool est_dossier(noeud*);
 extern bool est_nom_valide(char [100]);
 #endif
