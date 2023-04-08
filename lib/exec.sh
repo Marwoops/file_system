de9ee7603b03f8647ae28d8708d0bb62a2532adc
S1nj <marwan.azizi@gmail.com>
2023-04-08 18:39:31 +0200

    type instruction pour représenter les instructions entrées par l'utilisateur

--- lib/exec.sh
+++ lib/exec.sh
@@ -1,7 +1,8 @@
 gcc -Wall -c arbre.c
 gcc -Wall -c liste.c
+gcc -Wall -c instruction.c
 gcc -Wall -c main.c
-gcc -Wall -o main arbre.o liste.o main.o
+gcc -Wall -o main arbre.o liste.o main.o instruction.o
 ./main
 
 rm *.o
---
+++ lib/instruction.h
@@ -0,0 +1,33 @@
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+#include "arbre.h"
+
+struct instruction;
+
+enum commande {LS, CD, PWD, MKDIR, TOUCH, RM, CP, MV, PRINT};
+typedef enum commande commande;
+
+struct instruction {
+	commande cmd;
+	unsigned nombre_arguments;
+	char* arg1;
+	char* arg2;
+};
+
+typedef struct instruction instruction;
+
+extern instruction* generer_instruction(char*);
+extern char** get_chemin(char*);
+
+extern void ls(noeud*, instruction*);
+extern noeud* cd(noeud*, instruction*);
+extern void pwd(noeud*, instruction*);
+extern void mkdir(noeud*, instruction*);
+extern void touch(noeud*, instruction*);
+extern void rm(noeud*, instruction*);
+extern void cp(noeud*, instruction*);
+extern void mv(noeud*, instruction*);
+extern void print(noeud*, instruction*);
+
+#endif
