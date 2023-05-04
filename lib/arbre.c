e88ea4ace9b3f757faf9ccbbfc5b3bc393f55830
S1nj <marwan.azizi@gmail.com>
2023-05-04 11:40:04 +0200

    abstraction de la structure de chemin, corrections de bug, format.....

--- lib/arbre.c
+++ lib/arbre.c
@@ -7,14 +7,20 @@
 #include "exit.h"
 #include "debug.h"
 
-chemin *generer_chemin(char* noeuds) {
+struct chemin {
+	bool est_absolu;
+	size_t profondeur;
+	char** noeuds;
+};
+
+chemin *generer_chemin(char *noeuds) {
     chemin *chem = malloc(sizeof(chemin));
     if (chem == NULL) exit_malloc();
     chem->noeuds = decoupe_chemin(noeuds, &chem->est_absolu, &chem->profondeur);
     return chem;
 }
 
-noeud* creer_arbre() {
+noeud  *creer_arbre() {
 	flog("initialisation de l'arbre");
 	noeud* racine = malloc(sizeof(noeud));
 
@@ -29,7 +35,7 @@ noeud* creer_arbre() {
 	return racine;
 }
 
-noeud* creer_fichier(noeud* racine, char nom[100]) {
+noeud *creer_fichier(noeud *racine, noeud *pere, char nom[100]) {
 	flogf("création du fichier %s\n", nom);
 
 	noeud* fichier = malloc(sizeof(noeud));
@@ -38,107 +44,98 @@ noeud* creer_fichier(noeud* racine, char nom[100]) {
 
 	fichier->est_dossier = false;
 	fichier->racine = racine;
+	if (pere != NULL) {
+		fichier->pere = pere;
+		ajouter_elt(pere->fils, fichier);
+	}
+
 	memcpy(fichier->nom, nom, sizeof(char)*100);
 
 	return fichier;
 }
 
-noeud* creer_dossier(noeud* racine, char nom[100], liste_noeud* fils) {
+noeud *creer_dossier(noeud *racine, noeud *pere, char nom[100], liste_noeud *fils) {
 	flogf("création du dossier %s\n", nom);
 
-	noeud* dossier = malloc(sizeof(noeud));
+	noeud *dossier = malloc(sizeof(noeud));
 
 	if (dossier == NULL) exit_malloc();
 
 	dossier->est_dossier = true;
 	dossier->racine = racine;
 	dossier->fils = fils;
+	if (pere != NULL) {
+		dossier->pere = pere;
+		ajouter_elt(pere->fils, dossier);
+	}
+
 	memcpy(dossier->nom, nom, sizeof(char)*100);
 
 	return dossier;
 }
 
-noeud *ajouter_noeud(noeud *racine, chemin* chem, noeud *noeud) {
+noeud *ajouter_noeud(noeud *racine, chemin *chem, noeud *noeud) {
 	return racine;
 }
 
-char** decoupe_chemin(char *chemin, bool *est_absolu, size_t *taille) {
-	assert(chemin != NULL);
-	assert(*chemin != '\0');
+char **decoupe_chemin(char *chemin, bool *est_absolu, size_t *taille) {
+    assert(chemin != NULL);
+    assert(*chemin != '\0');
 
-	if (*chemin == '/') {
-		*est_absolu = true;
-		*chemin = *(chemin + 1);
-	} else {
-		*est_absolu = false;
-	}
+    if (*chemin == '/') {
+        *est_absolu = true;
+        *chemin = *(chemin + 1);
+    } else {
+        *est_absolu = false;
+    }
 
-	size_t nb_noeuds = 1;
-	size_t plus_long_noeud = 0;
-	size_t longueur_actuelle = 0;
-	size_t len = strlen(chemin);
+    size_t nb_noeuds = 1;
+    size_t len = strlen(chemin);
 
-	for (size_t i = 0; i < len; ++i) {
-		if (*(chemin + i) == '/') {
-			plus_long_noeud = (longueur_actuelle > plus_long_noeud) ?
-				longueur_actuelle : plus_long_noeud;
-			++nb_noeuds;
-			longueur_actuelle = 0;
-		} else {
-			++longueur_actuelle;
-		}
-	}
+    for (size_t i = 0; i < len; ++i) {
+        if (*(chemin + i) == '/') ++nb_noeuds;
+    }
 
-	*taille = nb_noeuds;
+    *taille = nb_noeuds;
 
-	plus_long_noeud = (longueur_actuelle > plus_long_noeud) ?
-		longueur_actuelle : plus_long_noeud;
+    char **chemin_decoupe = malloc((nb_noeuds+1) * sizeof(char*));
+    *chemin_decoupe = strdup(strtok(chemin, "/"));
 
-	char **chemin_decoupe = malloc(nb_noeuds * sizeof(char*));
-	*chemin_decoupe = malloc(plus_long_noeud * sizeof(char)+1);
-	strcpy(*chemin_decoupe, strtok(chemin, "/"));
+    for (size_t i = 1; i < nb_noeuds; ++i)
+        *(chemin_decoupe + i) = strdup(strtok(NULL, "/"));
 
-	for (size_t i = 1; i < nb_noeuds; ++i) {
-		*(chemin_decoupe+i) = malloc(plus_long_noeud * sizeof(char));
-		strcpy(*(chemin_decoupe+i), strtok(NULL, "/"));
-	}
-
-	return chemin_decoupe;
+    return chemin_decoupe;
 }
 
 noeud *aller_a(noeud *n, chemin *chem) {
-    assert(chem != NULL);
+	assert(chem != NULL);
     assert(n != NULL);
-	char q[3] = {'.', '.', '\0'};
+
     if (chem->est_absolu) {
         chem->est_absolu = false;
         flog("déplacement à la racine");
         return aller_a(n->racine, chem);
     }
 
-    if (chem->profondeur == 0) {
-        return n;
-    }
+    if (chem->profondeur == 0) return n;
 
     char *nom = *(chem->noeuds);
     chem->profondeur -= 1;
-    chem->noeuds = chem->noeuds + 1;
-	printf("%s\n",nom);
-	printf("%d\n",strcmp(nom, ".."));
-    if ((strcmp(nom, "..")==0) && n->pere != NULL) {
+	chem->noeuds = chem->noeuds + 1;
+
+    if ((strcmp(nom, "..") == 0) && n->pere != NULL) {
 	   flogf("déplacement vers le pere de %s\n", nom);
-	   printf("mega prooooooooooooouuuuuuuuuuuuuuut");
        return aller_a(n->pere, chem);
 	}
 
-	if (strcmp(nom, ".")==0) {
-       return aller_a(n,chem);
-	}
+	if (strcmp(nom, ".") == 0) return aller_a(n,chem);
+
     noeud *suivant = get_elt(n->fils, nom);
 
     // creer une erreur plus précise
-    if (suivant == NULL) exit_argument_null(suivant->nom);
+    if (suivant == NULL) exit_argument_invalide(nom);
     if (!suivant->est_dossier) exit_argument_invalide(suivant->nom);
+
     flogf("déplacement vers %s\n", nom);
     return aller_a(suivant, chem);
 }
--- lib/arbre.h
+++ lib/arbre.h
@@ -12,20 +12,14 @@ struct noeud {
 	struct liste_noeud *fils;
 };
 
-struct chemin {
-	bool est_absolu;
-	size_t profondeur;
-	char** noeuds;
-};
-
 typedef struct noeud noeud;
 typedef struct liste_noeud liste_noeud;
 typedef struct chemin chemin;
 
 extern chemin *generer_chemin(char*);
 extern noeud *creer_arbre();
-extern noeud *creer_fichier(noeud* , char [100]);
-extern noeud *creer_dossier(noeud* , char [100], liste_noeud*);
+extern noeud *creer_fichier(noeud*, noeud*, char [100]);
+extern noeud *creer_dossier(noeud*, noeud*, char [100], liste_noeud*);
 
 extern noeud *ajouter_noeud(noeud*, chemin*, noeud*);
 extern void supprimer_noeud(noeud*, chemin*, noeud*);
