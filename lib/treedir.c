00dd00bc7d432d65b9efd636d9ca5cd1aa8c22c6
S1nj <marwan.azizi@gmail.com>
2023-05-17 19:05:39 +0200

    malloc quand c'est nécessaire et nettoyage des tests

--- lib/treedir.c
+++ lib/treedir.c
@@ -13,22 +13,18 @@ bool obtenir_ligne(char **ligne, size_t *MAX_LENGTH, FILE *fichier) {
 	fgets(*ligne, *MAX_LENGTH, fichier);
 	if (feof(fichier)) return true;
 
-	//fgets(*ligne, *MAX_LENGTH, fichier);
-	char *tampon = malloc(257 * sizeof(char));
-	if (tampon == NULL) exit_malloc();
+	char *tampon = NULL;
 
 	while (strlen(*ligne) + 1 == *MAX_LENGTH) {
 		*MAX_LENGTH += 256;
 		*ligne = realloc(*ligne, *MAX_LENGTH);
-		if (*ligne == NULL) exit_malloc();
+		tampon = malloc(257 * sizeof(char));
+		if (*ligne == NULL || tampon == NULL) exit_malloc();
 		fgets(tampon, 257, fichier);
 		strcat(*ligne, tampon);
 	}
 
 	free(tampon);
-	//printf("On vient de lire %s|\n", *ligne);
-	//printf("%lu\n", strlen(*ligne));
-	//printf("aaaa %d\n", *(*ligne + strlen(*ligne)));
 	// on retire le saut de ligne
 	char *last = *ligne + strlen(*ligne)-1;
 	if (*last == '\n') *last = '\0';
